56c8f0adf9a8aee2788c9c63f87399cf7ec2a122
Ante Kristic <ante.kristic81@gmail.com>
2020-05-08 08:31:34 +0200

    Menu create

--- js/app.js
+++ js/app.js
@@ -17,24 +17,41 @@
  * Define Global Variables
  * 
 */
-
-
+const fragment = document.createDocumentFragment()
+const sectionList = document.querySelectorAll('section')
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
+function createNavItemHTML(id, name){
+    const itemHTML = `<a class ="menu__link" href='#${id}'>${name}</a>`;
+    return itemHTML;
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+function buildNavigation(){
+    for (let i=0; i < sectionList.length; i++){
+        const newMenuItem = document.createElement('li');
+        const sectionName = sectionList[i].getAttribute('data-nav')
+        const sectionId = sectionList[i].getAttribute('id')
+
+        newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
+        fragment.appendChild(newMenuItem);
+    }
+    const navBarList = document.getElementById('navbar__list')
+    navBarList.appendChild(fragment);
+}
+
+
+
 
 // build the nav
-
+buildNavigation()
 
 // Add class 'active' to section when near top of viewport
 
