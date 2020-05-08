bc250582a0843e66b845e52fc11de02198c22fc4
Ante Kristic <ante.kristic81@gmail.com>
2020-05-08 16:39:50 +0200

    Smooth scroll

--- js/app.js
+++ js/app.js
@@ -20,18 +20,17 @@
 const fragment = document.createDocumentFragment();
 const sectionList = document.querySelectorAll('section');
 
-
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 function createNavItemHTML(id, name){
-    const itemHTML = `<a class ="menu__link" href='#${id}'>${name}</a>`;
+    const itemHTML = `<a class ="menu__link" data-id="${id}">${name}</a>`;
     return itemHTML;
 }
 function isInViewport (elem) {
-    var bounding = elem.getBoundingClientRect();
+    const bounding = elem.getBoundingClientRect();
     return (
         bounding.top >= 0 &&
         bounding.left >= 0 &&
@@ -45,37 +44,40 @@ function isInViewport (elem) {
  * Begin Main Functions
  * 
 */
+
+// build the nav
 function buildNavigation(){
     for (let i=0; i < sectionList.length; i++){
         const newMenuItem = document.createElement('li');
         const sectionName = sectionList[i].getAttribute('data-nav')
         const sectionId = sectionList[i].getAttribute('id')
         newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
-        fragment.appendChild(newMenuItem);
+         fragment.appendChild(newMenuItem);
     }
     const navBarList = document.getElementById('navbar__list')
     navBarList.appendChild(fragment);
 }
 
+// Add class 'active' to section when near top of viewport
 function setActiveClass(){
     for (let i=0; i < sectionList.length; i++){
         if (isInViewport(sectionList[i])){
-            sectionList[i].className = "your-active-class";
+            sectionList[i].classList.add("your-active-class");
         }else{
-            sectionList[i].className = "";
+            sectionList[i].classList.remove("your-active-class");
         }
     }
 }
 
-
-
-// build the nav
-buildNavigation()
-
-// Add class 'active' to section when near top of viewport
-setActiveClass()
-
 // Scroll to anchor ID using scrollTO event
+function scrollToElement(event){
+    if(event.target.nodeName === 'A'){
+        const sectionId = event.target.getAttribute('data-id');
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({behavior: "smooth"});
+    }
+}
+
 
 
 /**
@@ -86,8 +88,12 @@ setActiveClass()
 document.addEventListener('scroll', function(){
     setActiveClass();
 });
+const navBarList = document.getElementById('navbar__list')
+navBarList.addEventListener('click', function(event){
+    scrollToElement(event)
+})
 // Build menu 
-
+buildNavigation()
 // Scroll to section on link click
 
 // Set sections as active
